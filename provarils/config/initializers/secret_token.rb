e46fa73aff795ae06de0109200252fb62825cf1d
Riccardo C <palladiusbonton@gmail.com>
2011-09-19 14:15:34 +0100

    Changes made by SparkleShare

---
+++ provarils/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Provarils::Application.config.secret_token = 'b3b7fa01c380e2d88e02332e5c795204c55dab2b5597a0d6da2136e9160e8affd5ebab00299862e90675d6becae9451cc1f893ab8bb0884981a50bc535a85407'
